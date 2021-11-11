6efe1f465658245ecc5198c547feb691283852f9
Michael Vineyard <michael.l.vineyard@gmail.com>
2021-11-11 16:22:08 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel = "stylesheet" href = "style.css">
+        <!-- index.php 
+        Michael Vineyard
+        vineyarm@csp.edu
+        Term Project
+        11/11/2021
+        -->
+    <head>
+        <title>Term Project</title>
+    </head>
+    <body>
+    <div class = "nav">
+            <div id = "buttons">
+                <button><a href = "readMe.html">Read Me</a></button> | <button><a href = "">Website</a></button>
+            </div>
+        </div>
+        <div class = "images">
+            <h3>Images for project</h3>
+        <a href="/termProject/Graphic/layout.png"><img src="/termProject/Graphic/layout.png"></a>
+        <a href = "/termProject/Graphic/database.png"><img src="/termProject/Graphic/database.png"></a>
+        </div>
+    </body>
+</html>
